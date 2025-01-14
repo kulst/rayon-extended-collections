57f364f35143a072d6df2624f0bf8890f2befb1c
kulst <60887784+kulst@users.noreply.github.com>
2025-01-14 04:17:09 +0100

    Added UnsafeCell to the type to prevent undefined behavior. Also added ManuallyDrop to as_vec()

--- src/once_vec.rs
+++ src/once_vec.rs
@@ -1,16 +1,18 @@
 use std::{
+    cell::UnsafeCell,
     collections::TryReserveError,
-    mem::MaybeUninit,
+    iter::repeat_n,
+    mem::{ManuallyDrop, MaybeUninit},
     sync::{
         atomic::{AtomicUsize, Ordering},
         Once,
     },
 };
-
+#[derive(Debug)]
 pub struct OnceVecError;
-
+#[derive(Debug)]
 pub struct OnceVec<T> {
-    vec: Vec<MaybeUninit<T>>,
+    vec: Vec<UnsafeCell<MaybeUninit<T>>>,
     once: Vec<Once>,
     elements_written: AtomicUsize,
 }
@@ -28,6 +30,16 @@ impl<T> OnceVec<T> {
         Self {
             vec: Vec::with_capacity(capacity),
             once: Vec::with_capacity(capacity),
+            elements_written: AtomicUsize::new(0),
+        }
+    }
+
+    pub fn with_uninit_len(len: usize) -> Self {
+        Self {
+            vec: repeat_n((), len)
+                .map(|_| UnsafeCell::new(MaybeUninit::uninit()))
+                .collect(),
+            once: repeat_n((), len).map(|_| Once::new()).collect(),
             elements_written: AtomicUsize::new(0),
         }
     }
@@ -88,7 +100,9 @@ impl<T> OnceVec<T> {
             // Also `MaybeUninit` is guaranteed to have the same layout as `T`.
             // The pointer is valid since it refers to memory owned by us which is a
             // reference and thus guaranteed to be valid for reads.
-            let slice = unsafe { &*(self.vec.as_slice() as *const [MaybeUninit<T>] as *const [T]) };
+            let slice = unsafe {
+                &*(self.vec.as_slice() as *const [UnsafeCell<MaybeUninit<T>>] as *const [T])
+            };
             Ok(slice)
         }
     }
@@ -99,8 +113,9 @@ impl<T> OnceVec<T> {
         } else {
             // SAFETY: similar to safety notes for `as_slice`, but we have a
             // mutable reference which is also guaranteed to be valid for writes.
-            let slice =
-                unsafe { &mut *(self.vec.as_mut_slice() as *mut [MaybeUninit<T>] as *mut [T]) };
+            let slice = unsafe {
+                &mut *(self.vec.as_mut_slice() as *mut [UnsafeCell<MaybeUninit<T>>] as *mut [T])
+            };
             Ok(slice)
         }
     }
@@ -109,9 +124,10 @@ impl<T> OnceVec<T> {
         if !self.is_fully_written() {
             Err(OnceVecError)
         } else {
-            let ptr = self.vec.as_mut_ptr() as *mut T;
-            let length = self.vec.len();
-            let capacity = self.vec.capacity();
+            let mut v = ManuallyDrop::new(self.vec);
+            let ptr = v.as_mut_ptr() as *mut T;
+            let length = v.len();
+            let capacity = v.capacity();
             // SAFETY: We own `vec` so `ptr` results from an allocation with the global
             // allocator. `MaybeUninit` is guarantied to have the same layout as `T`.
             // Length and capacity are directly resulting from a safe vector so length
@@ -122,7 +138,8 @@ impl<T> OnceVec<T> {
     }
 
     pub fn insert(&mut self, index: usize, element: T) {
-        self.vec.insert(index, MaybeUninit::new(element));
+        self.vec
+            .insert(index, UnsafeCell::new(MaybeUninit::new(element)));
         let once = Once::new();
         let mut once_check = false;
         once.call_once(|| once_check = true);
@@ -132,7 +149,7 @@ impl<T> OnceVec<T> {
     }
 
     pub fn insert_uninit(&mut self, index: usize, element: MaybeUninit<T>) {
-        self.vec.insert(index, element);
+        self.vec.insert(index, UnsafeCell::new(element));
         self.once.insert(index, Once::new());
     }
 
@@ -142,7 +159,7 @@ impl<T> OnceVec<T> {
         if once.is_completed() {
             self.elements_written.fetch_sub(1, Ordering::Relaxed);
             // SAFETY: We checked that the value was written before
-            Some(unsafe { val.assume_init() })
+            Some(unsafe { val.into_inner().assume_init() })
         } else {
             None
         }
@@ -153,11 +170,11 @@ impl<T> OnceVec<T> {
         if self.once.remove(index).is_completed() {
             self.elements_written.fetch_sub(1, Ordering::Relaxed);
         }
-        val
+        val.into_inner()
     }
 
     pub fn push(&mut self, value: T) {
-        self.vec.push(MaybeUninit::new(value));
+        self.vec.push(UnsafeCell::new(MaybeUninit::new(value)));
         let once = Once::new();
         let mut once_check = false;
         once.call_once(|| once_check = true);
@@ -167,7 +184,7 @@ impl<T> OnceVec<T> {
     }
 
     pub fn push_uninit(&mut self, value: MaybeUninit<T>) {
-        self.vec.push(value);
+        self.vec.push(UnsafeCell::new(value));
         self.once.push(Once::new());
     }
 
@@ -178,7 +195,7 @@ impl<T> OnceVec<T> {
             if once.is_completed() {
                 self.elements_written.fetch_sub(1, Ordering::Relaxed);
                 // SAFETY: We checked that the value was written before
-                Some(unsafe { val.unwrap().assume_init() })
+                Some(unsafe { val.unwrap().into_inner().assume_init() })
             } else {
                 None
             }
@@ -194,7 +211,22 @@ impl<T> OnceVec<T> {
                 self.elements_written.fetch_sub(1, Ordering::Relaxed);
             }
         }
-        val
+        Some(val.unwrap().into_inner())
+    }
+
+    pub fn try_write(&self, index: usize, val: T) -> Result<(), OnceVecError> {
+        let mut once_check = false;
+        self.once[index].call_once(|| unsafe {
+            let element = self.vec[index].get() as *mut T;
+            *element = val;
+            once_check = true;
+        });
+        if once_check == true {
+            self.elements_written.fetch_add(1, Ordering::Relaxed);
+            Ok(())
+        } else {
+            Err(OnceVecError)
+        }
     }
 }
 
@@ -214,4 +246,27 @@ impl<T> OnceVec<T> {
     fn is_fully_written(&self) -> bool {
         self.elements_written.load(Ordering::Relaxed) == self.once.len()
     }
+}
+
+#[test]
+fn first_test() {
+    let mut once_vec: OnceVec<f32> = OnceVec::with_uninit_len(6);
+    let _ = once_vec.try_write(5, 0.35);
+    assert!(once_vec.as_slice().is_err());
+    let _ = once_vec.try_write(0, 0.30);
+    let _ = once_vec.try_write(1, 0.31);
+    let _ = once_vec.try_write(2, 0.32);
+    let _ = once_vec.try_write(3, 0.33);
+    let _ = once_vec.try_write(4, 0.34);
+    assert_eq!(
+        once_vec.as_slice().unwrap(),
+        &[0.30, 0.31, 0.32, 0.33, 0.34, 0.35]
+    );
+    once_vec.as_mut_slice().unwrap()[5] = 0.40;
+    assert_eq!(
+        once_vec.as_slice().unwrap(),
+        &[0.30, 0.31, 0.32, 0.33, 0.34, 0.40]
+    );
+    let vec = once_vec.as_vec().unwrap();
+    assert_eq!(vec, vec![0.30, 0.31, 0.32, 0.33, 0.34, 0.40]);
 }
